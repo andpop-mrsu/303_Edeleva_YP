6e57a6df33caff4c28e6a774f26b4344dbaf25d7
Edeleva Yulia <edelewa.yulia@yandex.ru>
2021-02-20 10:54:22 +0300

    activate .bat file

--- Task02/make_db_init.py
+++ Task02/make_db_init.py
@@ -36,27 +36,24 @@ def separator(s):
 with open("db_init.sql", "w", encoding="utf-8") as fout:
     print(".open movies_rating.db\n", file=fout)
 
-    # удаление таблиц, если они уже существуют
-    print("DROP TABLE IF EXISTS movies", end="\n", file=fout)
-    print("DROP TABLE IF EXISTS ratings", end="\n", file=fout)
-    print("DROP TABLE IF EXISTS tags", end="\n", file=fout)
-    print("DROP TABLE IF EXISTS users", end="\n", file=fout)
+     # удаление таблиц, если они уже существуют
+    print("DROP TABLE IF EXISTS movies;\n", file=fout)
+    print("DROP TABLE IF EXISTS ratings;\n", file=fout)
+    print("DROP TABLE IF EXISTS tags;\n", file=fout)
+    print("DROP TABLE IF EXISTS users;\n", file=fout)
 
-    # создание таблиц
-    print("CREATE TABLE movies (id INTEGER RRIMARY KEY, title VARCHAR, year INTEGER, genres VARCHAR)", end="\n",
+     # создание таблиц
+    print("CREATE TABLE movies (id INTEGER RRIMARY KEY, title VARCHAR, year VARCHAR, genres VARCHAR);\n",
           file=fout)
-    print(
-        "CREATE TABLE ratings (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, rating FLOAT, timestamp INTEGER)",
-        end="\n", file=fout)
-    print(
-        "CREATE TABLE tags (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, tag VARCHAR, timestamp INTEGER)",
-        end="\n", file=fout)
-    print(
-        "CREATE TABLE users (id INTEGER RRIMARY KEY, name VARCHAR, email VARCHAR, gender VARCHAR, register_date DATE, occupation VARCHAR)",
-        end="\n", file=fout)
+    print("CREATE TABLE ratings (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, rating FLOAT, timestamp INTEGER);\n", 
+          file=fout)
+    print("CREATE TABLE tags (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, tag VARCHAR, timestamp INTEGER);\n", 
+          file=fout)
+    print("CREATE TABLE users (id INTEGER RRIMARY KEY, name VARCHAR, email VARCHAR, gender VARCHAR, register_date DATE, occupation VARCHAR);\n", 
+          file=fout)
 
     # парсинг файла movies.csv и заполнение таблицы movies
-    print("INSERT INTO movies (id, title, year, genres)", end="\n", file=fout)
+    print("INSERT INTO movies (id, title, year, genres)\n", file=fout)
     print(f"VALUES ", file=fout)
 
     with open("movies.csv", "r", encoding="utf-8") as f:
@@ -70,7 +67,7 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
                 print("({}, '{}', {}, '{}'),\n".format(item[0], item[3], item[2], item[1]), file=fout)
             else:
 
-                print("({}, '{}', {}, '{}'),\n".format(item[0], item[3], item[2], item[1]), file=fout)
+                print("({}, '{}', {}, '{}');\n".format(item[0], item[3], item[2], item[1]), file=fout)
 
                 # парсинг файла ratings.csv и заполнение таблицы ratings
     print("INSERT INTO ratings (id, user_id, movie_id, rating, timestamp)\n", file=fout)
@@ -84,8 +81,8 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
 
                 print("({},{}, {}, {}, {}),\n".format(i, item[0], item[1], item[2], item[3]), file=fout)
             else:
-                print(f"({i}, {item[0]}, {item[1]}, {item[2]}, {item[3]});\n", file=fout)
-                print("({},{}, {}, {}, {}),\n".format(i, item[0], item[1], item[2], item[3]), file=fout)
+               
+                print("({},{}, {}, {}, {});\n".format(i, item[0], item[1], item[2], item[3]), file=fout)
                 # парсинг файла tags.csv и заполнение таблицы tags
     print("INSERT INTO tags (id, user_id, movie_id, tag, timestamp)\n", file=fout)
     print(f"VALUES", end=" ", file=fout)
@@ -101,24 +98,20 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
                 print("({}, {}, {}, '{}', {}),\n".format(i, item[0], item[1], item[2], item[3]), file=fout)
             else:
 
-                print("({},{}, {}, '{}', {}),\n".format(i, item[0], item[1], item[2], item[3]), file=fout)
+                print("({},{}, {}, '{}', {});\n".format(i, item[0], item[1], item[2], item[3]), file=fout)
 
-                # парсинг файла users.txt и заполнение таблицы users
+               # парсинг файла users.txt и заполнение таблицы users            
     print("INSERT INTO users (id, name, email, gender, register_date, occupation)\n", file=fout)
     print(f"VALUES", end=" ", file=fout)
-
+    
     with open("users.txt", "r") as fl:
         file = fl.readlines()[1:]
         for i in range(len(file)):
             item = file[i][:-1]
             item = item.replace("'", "‘")
             item = item.split("|")
-            if i != len(file) - 1:
-
-                print("({}, '{}', '{}', '{}' {}, '{}'),\n".format(item[0], item[1], item[2], item[3], item[4], item[5]),
-                      file=fout)
+            if i != len(file)-1:
+                print("( {}, '{}', '{}', '{}', {}, '{}'),\n".format(item[0], item[1], item[2], item[3], item[4],item[5] ), file=fout)
             else:
-
-                print("({}, '{}', '{}', '{}' {}, '{}'),\n".format(item[0], item[1], item[2], item[3], item[4], item[5]),
-                      file=fout)
-
+                 print("( {}, '{}', '{}', '{}', {}, '{}');\n".format(item[0], item[1], item[2], item[3], item[4],item[5] ), file=fout)
+                
